7167ddbdcbbbc37888801d73892c3f479adb5eb8
JangDaniel <csbon00@gmail.com>
2019-07-24 14:10:54 +0900

    initial

---
+++ insnet/src/main/java/com/insutil/insnet/InsnetApplication.java
@@ -0,0 +1,13 @@
+package com.insutil.insnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsnetApplication.class, args);
+	}
+
+}
---
+++ insnet/src/test/java/com/insutil/insnet/InsnetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.insutil.insnet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InsnetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
